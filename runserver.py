76d4cfd5eb95052bda2272e933060020b5a9f58d
spout <snoopyxdy@gmail.com>
2014-06-05 08:48:12 +0800

    11

--- runserver.py
+++ runserver.py
@@ -8,6 +8,43 @@ import os
 import sys
 import getopt
 
+
+
+
+def mkdir(path):
+    # 引入模块
+    
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+ 
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists=os.path.exists(path)
+ 
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        print path+' create success'
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print path+' is exist'
+        return False
+
+
+#计算路径，然后创建文件夹
+curPath = os.path.split(os.path.realpath(__file__))[0]
+logsPath = curPath + os.sep + 'vipsystem' + os.sep + 'logs'
+
+mkdir(logsPath)
+
+
+
 __opts, _ = getopt.getopt(sys.argv[1:], "e:") #获取命令行参数
 __scritpEnv = ""
 
